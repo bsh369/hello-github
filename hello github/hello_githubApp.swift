d613f262d41938850122c2841da72c110904a995
bsh369 <ivenus54@gmail.com>
2024-09-26 17:03:57 +0300

    Initial Commit

---
+++ hello github/hello_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_githubApp.swift
+//  hello github
+//
+//  Created by Bshayr369 on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct hello_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hello githubTests/hello_githubTests.swift
@@ -0,0 +1,17 @@
+//
+//  hello_githubTests.swift
+//  hello githubTests
+//
+//  Created by Bshayr369 on 23/03/1446 AH.
+//
+
+import Testing
+@testable import hello_github
+
+struct hello_githubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
